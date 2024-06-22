a966c657009bafebc8dbcda0ab8e66a07ff646ff
James Wyndham <samwyn@xam.com.au>
2024-06-22 13:11:37 +0800

    feature - added map + incidents

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./pages/Search";
-import Map from "./pages/Map";
+import Map from "./pages/DefinatelyNotThis";
+import MobileMap from "./pages/MobileMap";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/mobilemap" element={<MobileMap />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/DefinatelyNotThis.tsx
@@ -0,0 +1,41 @@
+import { defineCustomElements } from "@arcgis/map-components/dist/loader";
+import MapView from "../components/MapView";
+
+function Map() {
+  defineCustomElements(window, {
+    resourcesUrl: "https://js.arcgis.com/map-components/4.29/assets",
+  });
+  return (
+    <div>
+      <header className="flex justify-between items-center bg-white">
+        <h1 className="text-2xl font-bold">Blueflare</h1>
+        <nav>
+          <ul className="flex list-none">
+            <li className="mr-2.5">Menu Item 1</li>
+            <li className="mr-2.5">Menu Item 2</li>
+            <li className="mr-2.5">Menu Item 3</li>
+          </ul>
+        </nav>
+        <div className="flex mr-2.5">
+          <button className="bg-black text-white px-4 py-2 rounded mr-6">
+            Share
+          </button>
+          <button className="flex items-center justify-center rounded-full bg-gray-200 h-10 w-10 overflow-hidden">
+            <img
+              src="/path/to/profile-pic.jpg"
+              alt="Profile"
+              className="h-full w-full object-cover"
+            />
+          </button>
+        </div>
+      </header>
+      <div style={{ width: "100%", display: 'flex' }}>
+        <div style={{ width: "20rem" }}>Side</div>
+        <MapView />
+        <div style={{ width: "20rem" }}>Side</div>
+      </div>
+    </div>
+  );
+}
+
+export default Map;
