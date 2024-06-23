f90d697ae6fea85692141bb8b4deb0368bfbee01
kelvinh <kevdagoat@gmail.com>
2024-06-23 11:26:09 +0800

    bug(be): db

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,14 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import { DB_URL } from "./config";
 
 const pool = new Pool({
-  connectionString: "postgres://postgres:postgres@localhost:5432/postgres",
+    connectionString: DB_URL,
 });
+
+pool.connect()
+    .then(() => {
+        console.log("Connected to Postgres");
+    })
 
 export const db = drizzle(pool);
